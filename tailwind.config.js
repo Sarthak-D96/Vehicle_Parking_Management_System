7d9dfbf54aabf931afd9877094d5f5190e814c79
Sarthak-D96 <sduvedi_be24@thapar.edu>
2026-05-03 23:43:05 +0530

    Enhance tailwind.config.js with explanatory comments
    Added comments to explain Tailwind CSS configuration.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,10 @@
+
+// Tailwind CSS configuration file for the Smart Parking System application. 
+// This file specifies the content sources for Tailwind to scan for class names, 
+// as well as any custom theme extensions and plugins. 
+// The configuration ensures that Tailwind generates the necessary CSS based on the classes used in the application's views and JavaScript files, 
+// allowing for a consistent and efficient styling approach throughout the application.
+
 module.exports = {
   content: [
     './views/**/*.ejs',
